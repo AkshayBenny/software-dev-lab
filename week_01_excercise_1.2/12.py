d70bde8f33049023aa93564803dc9411bc215bfd
Akshay Benny <akshay@mx-zenbook>
2023-09-20 20:12:00 +0530

    removed unwanted comments

--- week_01_excercise_1.2/12.py
+++ week_01_excercise_1.2/12.py
@@ -5,7 +5,5 @@ myDictionary = {
 
 print(myDictionary['mname'])
 
-# Traceback (most recent call last):
-#   File "/home/akshay/Projects/study/software-dev-lab/week_01_excercise_1.2/12.py", line 6, in <module>
-#     print(myDictionary['mname'])
+
 # KeyError: 'mname'
