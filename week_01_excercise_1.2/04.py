4698cddc7553def32ed27dfdb73d6fc377f6bb87
Akshay Benny <akshay@mx-zenbook>
2023-09-20 20:15:36 +0530

    edit

--- week_01_excercise_1.2/04.py
+++ week_01_excercise_1.2/04.py
@@ -1,7 +1,13 @@
 authors = {
     'John Doe': 4,
     'Jane Doe': 6,
+    'Alex': 6,
+    'Benny': 6,
 }
 
+num = 0
+
 for key, value in authors.items():
-    print(key, value)
+    if (num < 2):
+        print(key, value)
+    num += 1
