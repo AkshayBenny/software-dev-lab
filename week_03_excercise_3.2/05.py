cc0d175ffb0437e0c4650f0cb6adf2b94197ba62
Akshay Benny <akshay@mx-zenbook>
2023-10-02 19:21:06 +0530

    edit

--- week_03_excercise_3.2/05.py
+++ week_03_excercise_3.2/05.py
@@ -1,8 +1,8 @@
 def strToInt(strNum):
     try:
         print(int(strNum) + 10)
-    except:
-        print("Error")
+    except ValueError as e:
+        print(f"Error {e} {e.__class__.__name__}")
 
 
 strToInt("4asdsd")
