4784c854697c99c9daa0bc0585e3d5be54f8475b
Akshay Benny <akshay@mx-zenbook>
2023-10-16 17:46:31 +0530

    edit

--- week_05_excercise_5.2/01.py
+++ week_05_excercise_5.2/01.py
@@ -2,16 +2,31 @@ my_string = 'Hello world from the University of Strathclyder'
 
 
 def char_freq_counter(string):
-    char_dict = {}
+    char_freq_dict = {}
     for char in string.replace(" ", ""):
-        if char in char_dict:
-            char_dict[char.lower()] += 1
+        if char in char_freq_dict:
+            char_freq_dict[char.lower()] += 1
         else:
-            char_dict[char.lower()] = 1
+            char_freq_dict[char.lower()] = 1
 
-    return char_dict
+    return char_freq_dict
 
 
 new_char_dict = char_freq_counter(my_string)
 
-print(f'{new_char_dict}')
+print(f'Character counter: {new_char_dict}')
+
+
+def vowel_freq_counter(string):
+    vowel_freq_dict = {}
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for char in string.replace(" ", ""):
+        if (char.lower() in vowels):
+            if char in vowel_freq_dict:
+                vowel_freq_dict[char.lower()] += 1
+            else:
+                vowel_freq_dict[char.lower()] = 1
+    return vowel_freq_dict
+
+
+print(f'Vowel counter: {vowel_freq_counter(my_string)}')
